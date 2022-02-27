53e220759437777195fc1b0b7fe36019a3a8711d
Fuiiyoh <100391116+Fuiiyoh@users.noreply.github.com>
2022-02-28 08:44:16 +1300

    yes / no checker

---
+++ 01_yes_no.py
@@ -0,0 +1,16 @@
+show_instructions = ""
+while show_instructions.lower() != "xxx":
+  
+  # asks if the user has played before
+  show_instructions = input("Have you played this game before? ").lower()
+  
+  # if they say yes, output 'program continues'
+  if show_instructions == ("yes") or show_instructions == ("y"):
+    show_instructions == ("yes")
+    print("program continues")
+  
+  elif show_instructions == ("no") or show_instructions == ("n"):
+    print("Display instructions")
+  #if they say no, output 'display instructions'
+  else:
+    print("Please answer either yes / no")
